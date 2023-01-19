1ecd7c23f8cef8db3651e462453826ab99af2e41
Miguel Guuajardo <miguelguajardo1468@hotmail.com>
2023-01-19 13:13:44 -0300

    Correccion de errores

--- src/controllers/sessionControllers/index.js
+++ src/controllers/sessionControllers/index.js
@@ -1,10 +1,13 @@
 const cookieParser = require("cookie-parser")
 
 const logInUser = (req, res) => {
-    try {
-        res.render('login.hbs')
-    } catch (error) {
-        console.log(`error from logInUser - ${error}`);
+    const userName = req.session.user
+    if(req.session?.user){
+        res.render('products.hbs',{userName})
+        console.log(`se conectó el usuario ${userName}`)
+    }else{
+        res.render("login.hbs")
+        console.log("Por favor rellene el campo usuario")
     }
 }
 
@@ -32,21 +35,11 @@ const logOutRedirect = (req,res)=>{
 }
 
 const postLogIn = (req, res) => {
-    try {
         const userName = req.body.user
         const sessionId = req.session.id
         req.session.user = userName
         console.log('id de session: ', sessionId);
-        if(req.session.user){
-            res.render('products.hbs',{userName})
-            console.log(`se conectó el usuario ${userName}`)
-        }else{
-            res.render("login.hbs")
-            console.log("Por favor rellene el campo usuario")
-        }
-    } catch (error) {
-        console.log(`Error desde el postLogIn`, error);
-    }
+        res.redirect("/login")
 }
 
 module.exports = {logInUser,logOutUser,postLogIn,logOutRedirect}
